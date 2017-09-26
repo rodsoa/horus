7dfaa3b2e551046dcfa1d939f7ba795acdd5fe6a
Rodrigo Soares Souza <rodsoadev@gmail.com>
2017-09-26 17:30:21 -0300

    alteração em temp para dompdf

--- vendor/barryvdh/laravel-dompdf/config/dompdf.php
+++ vendor/barryvdh/laravel-dompdf/config/dompdf.php
@@ -57,7 +57,7 @@ return array(
          * The temporary directory is required to download remote images and when
          * using the PFDLib back end.
          */
-        "DOMPDF_TEMP_DIR" => sys_get_temp_dir(),
+        "DOMPDF_TEMP_DIR" => storage_path('tmp/'),
 
         /**
          * ==== IMPORTANT ====
