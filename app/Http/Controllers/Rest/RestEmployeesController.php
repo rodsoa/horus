fb5c4350e0723ba347930ad084310a6498cc44d0
Rodrigo Soares Souza <rodsoadev@gmail.com>
2018-01-08 00:29:28 -0300

    ...

--- app/Http/Controllers/Rest/RestEmployeesController.php
+++ app/Http/Controllers/Rest/RestEmployeesController.php
@@ -15,9 +15,7 @@ class RestEmployeesController extends Controller
         // TODO: ADICIONAR DATA NO WORKSCHEDULE
         $events = [];
         $cont = 0;
-
-        return $employee->work_schedules;
-
+        
         foreach ($employee->work_schedules as $key => $ws) {
             $events[$cont]['title'] = $ws->building->name ;
             $events[$cont]['start'] = $ws->date .' '. explode(" ", explode("-",$ws->schedule->time_range)[0])[0];
